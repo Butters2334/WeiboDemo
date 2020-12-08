a054a741c3de93833ade105083c8077cd38be89d
butters <mhk466@163.com>
2020-12-08 18:05:06 +0800

    [☠️]update

--- WeiboDemo/Demo/SDImgeDemo.swift
+++ WeiboDemo/Demo/SDImgeDemo.swift
@@ -9,13 +9,58 @@
 import SwiftUI
 
 struct SDImgeDemo: View {
+    let url : URL?
+    @State private var data: Data?
+    private var image: UIImage? {
+        if let data = self.data {
+            return UIImage(data: data)
+        }
+        return nil
+    }
+    @State private var loading:Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let image = self.image
+        return VStack{
+            Group {
+                if image != nil {
+                    Image(uiImage: image!)
+                        .resizable()
+                        .scaledToFill()
+                        .clipped()
+                }else{
+                    Color.gray
+                }
+            }
+            .frame(height:300)
+            .overlay(
+                ActivityIndicator(isAnimating: $loading, style: .large)
+                    .foregroundColor(.red)
+            )
+            Button(action: {
+                if let url = self.url{
+                    DispatchQueue.global().async {
+                        DispatchQueue.main.async {
+                            self.loading = true
+                            self.data = Data()
+                        }
+                        guard let data = try? Data(contentsOf: url) else{
+                            return
+                        }
+                        DispatchQueue.main.async {
+                            self.loading = false
+                            self.data = data
+                        }
+                    }
+                }
+            }) {
+                Text("down image")
+            }//.onAppear(perform: T##(() -> Void)?##(() -> Void)?##() -> Void)
+        }
     }
 }
 
 struct SDImgeDemo_Previews: PreviewProvider {
     static var previews: some View {
-        SDImgeDemo()
+        SDImgeDemo(url: URL(string: "https://raw.githubusercontent.com/anmac/WeiboDemo/master/WeiboDemo/Resources/4e7f0c83gy1gam2misv31j21hc0u016k.jpg"))
     }
 }
