23ffb022562809d5d5f6883abd5504a33fe7fe73
butters2334 <mahuajian@gmail.com>
2020-12-18 16:47:49 +0800

    [😁]debug

--- WeiboDemo/Demo/TapDemo.swift
+++ WeiboDemo/Demo/TapDemo.swift
@@ -10,50 +10,83 @@ import SwiftUI
 
 struct TapDemo: View {
     var body: some View {
-        VStack(spacing:0.0){
-//            ModifiedContent(content: Text("tap-a").font(.system(size:30)), modifier: TestModifier())
-//            Text("tap-b")
-//                .font(.system(size:30))
-//                .background(Color(UIColor{ trait -> UIColor in
-//                    .lightGray
-//                }))
-//                .modifier(TestModifier())
-//                .modifier(ColorStyle())
-//            .setColor()
-            Text("Title")
-                .textStyle(.title)
-//                .modifier(ColorStyle())
-            Text("subtitle")
-                .textStyle(.subtitle)
-                .padding(10.0)
-//            Image(systemName: "moon.zzz")
-//                .frame(width: 100, height: 100)
-//                .font(.system(size: 30, weight: .bold))
-//            Image(systemName: "sun.max")
-//                .frame(width: 100, height: 100)
-//                .font(.system(size: 30, weight: .light))
-//            TextField("", text: .constant(""), onEditingChanged: { (b) in
-//
-//            }) {
-//
-//            }
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                .padding(.leading,30)
-//                .padding(.trailing,100)
-//
-//            SecureField("test", text: .constant(""))
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                .padding(.leading,30)
-//                .padding(.trailing,100)
-            
-            Image("1024")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 200, height: 200, alignment: .center)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.red, lineWidth: 10)
-            )
+        HStack(alignment: .top){
+            VStack(spacing:0.0){
+    //            ModifiedContent(content: Text("tap-a").font(.system(size:30)), modifier: TestModifier())
+    //            Text("tap-b")
+    //                .font(.system(size:30))
+    //                .background(Color(UIColor{ trait -> UIColor in
+    //                    .lightGray
+    //                }))
+    //                .modifier(TestModifier())
+    //                .modifier(ColorStyle())
+    //            .setColor()
+                Text("Title")
+                    .textStyle(.title)
+    //                .modifier(ColorStyle())
+                Text("subtitle")
+                    .textStyle(.subtitle)
+                    .padding(10.0)
+    //            Image(systemName: "moon.zzz")
+    //                .frame(width: 100, height: 100)
+    //                .font(.system(size: 30, weight: .bold))
+    //            Image(systemName: "sun.max")
+    //                .frame(width: 100, height: 100)
+    //                .font(.system(size: 30, weight: .light))
+    //            TextField("", text: .constant(""), onEditingChanged: { (b) in
+    //
+    //            }) {
+    //
+    //            }
+    //            .textFieldStyle(RoundedBorderTextFieldStyle())
+    //                .padding(.leading,30)
+    //                .padding(.trailing,100)
+    //
+    //            SecureField("test", text: .constant(""))
+    //            .textFieldStyle(RoundedBorderTextFieldStyle())
+    //                .padding(.leading,30)
+    //                .padding(.trailing,100)
+                
+                Image("1024")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .border(Color.green, width: 10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.red, lineWidth: 10)
+                    )
+                    .hidden()
+                    .frame(height:0)
+                
+                ZStack{
+                    Text("\t\t\t\t").background(Color.red)
+                        .cornerRadius(10)
+                        .border(Color.green, width: 1)
+                    Divider()
+                    Text("asasddsasd")
+                }.frame(height:30)
+                HStack(spacing:60){
+                    VStack{
+                        ForEach(0..<3){
+                            Text("\($0)")
+                        }
+                    }
+                    VStack{
+                        ForEach((1...3).map{$0},id: \.self){
+                            Text("\($0)")
+                            .bold()
+                        }
+                    }
+                    .border(Color.red)
+                    .cornerRadius(10)
+                }
+                Group{
+                    ForEach(0..<3){
+                        Text("True\($0)")
+                    }
+                }
+            }
         }
     }
 }
